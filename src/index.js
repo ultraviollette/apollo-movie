39f4b197f17123580c489b3df162ddfec0885561
Melissa Lee <plhw90@gmail.com>
2021-01-01 17:32:40 -0700

    Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./apollo";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
