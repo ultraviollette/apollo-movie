556a88bce880a678dbc2f5d4b4ef34a821ea4225
Melissa Lee <plhw90@gmail.com>
2021-01-02 17:42:26 -0700

    GET MOVIE query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,31 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  //   console.log(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  //   console.log(loading, data);
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+  return "Detail";
+};
